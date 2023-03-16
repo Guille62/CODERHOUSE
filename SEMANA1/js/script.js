a2abf868a0ab158272161512205469cc491df285
Guille62 <gtineo62@gmail.com>
2023-03-16 00:38:41 -0300

    semana 1

---
+++ SEMANA1/js/script.js
@@ -0,0 +1,7 @@
+let  anoActual = 2023;
+let  nombre = prompt('Ingresa tu nombre');
+let anoNacimiento = parseFloat(prompt('Ingresa tu año de nacimiento'));
+
+let edadActual = anoActual - anoNacimiento;
+
+alert('Hola ' + nombre + ' tu edad actual es ' + edadActual + ' años');
